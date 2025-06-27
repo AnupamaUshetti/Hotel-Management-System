121299935bf5236dfa626fd725232931534e30ff
Anupama Ushetti <anupamaush69@gmail.com>
2025-06-27 13:59:15 +0530

    Add a Guest calss

--- src/Guest.java
+++ src/Guest.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Guest {
+
+    private String name;
+    private String surname;
+    private int roomNumber;
+    private int nightStayed;
+
+    public Guest (String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setRoomNumber(int roomNumber){
+        this.roomNumber = roomNumber;
+    }
+    public void setNightSayed(int nightStayed){
+        this.nightStayed = nightStayed;
+    }
+
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+    public int getNightStayed(){
+        return nightStayed;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + surname + " " + roomNumber + " " + nightStayed;
+    }
 }

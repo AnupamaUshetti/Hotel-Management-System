8b06b5e18594ccf3d6d22bd059a1bab3eaa194fd
Anupama Ushetti <anupamaush69@gmail.com>
2025-06-27 10:30:39 +0530

    Add a HouseKeeper class

--- src/HouseKeeper.java
+++ src/HouseKeeper.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class HouseKeeper {
+public class HouseKeeper extends HotelStaff{
+    private int yearsOfExperience;
+
+    public HouseKeeper(String name, String surname, LocalDate dateOfBirth, int yearsOfExperience){
+        super(name, surname);
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience){
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public int getYearsOfExperience(){
+        return yearsOfExperience;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " Housekeeper - Years of Experience: " + yearsOfExperience;
+    }
 }

7ddf1f53656221221c1c0d3fefe856d58c4e4aca
Anupama Ushetti <anupamaush69@gmail.com>
2025-06-27 10:48:43 +0530

    Add a Manager class

--- src/Manager.java
+++ src/Manager.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Manager extends HotelStaff{
+    private String licenceNumber;
 
-public class Manager {
+    public Manager(String name, String surname, String licenceNumber){
+        super(name, surname);
+        this.licenceNumber = licenceNumber;
+    }
+
+    public void setLicenceNumber(String licenceNumber){
+        this.licenceNumber = licenceNumber;
+    }
+
+    public String getLicenceNumber(){
+        return licenceNumber;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " Licence Number: " + licenceNumber + "\n";
+    }
 }

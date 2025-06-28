d57e1c9ae429670ff8339bc07c0448de2ac88366
Anupama Ushetti <anupamaush69@gmail.com>
2025-06-28 23:43:30 +0530

    Create a new class called WestminsterHotelManager

---
+++ src/WestminsterHotelManager.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class WestminsterHotelManager {
+    private ArrayList<HotelStaff> hotelStaffList;
+    private ArrayList<Guest> hotelGuestList;
+    private int staffLimit;
+
+    public WestminsterHotelManager() {
+        hotelStaffList = new ArrayList<>();
+        hotelGuestList = new ArrayList<>();
+        staffLimit = maxMembersNumber;
+
+    }
+}
